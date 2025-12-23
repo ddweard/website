a8c70d666c19ab983ed1816ad2bb56deb343ff23
ddweard <timurnar2005@gmail.com>
2025-12-23 10:23:15 +0300

    10

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<form action="index.php" method="post">
-    <label>Radius:</label><br>
-    <input type="text" name="radius"><br>
-    <input type="submit" value="Calculate">
-</form>
-
-
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $radius = $_POST['radius'];
-    $circumference = round(2 * pi() * $radius, 2);
-    $area = round(pi() * pow($radius, 2), 2);
-    $volume = round((4/3) * pi() * pow($radius, 3), 2);
-    echo "Circumference: {$circumference} cm<br>";
-    echo "Area: {$area} cm²<br>";
-    echo "Volume: {$volume} cm³<br>";
+$age = 170;
+if ($age >= 18 && $age < 100) {
+    echo "You may enter the site.";
+} else if ($age >= 100) {
+    echo "You are too old to enter.";
+} else {
+    echo "You must be 18+ to enter.";
 }
 ?>
