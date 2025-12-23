0b382e9dad99bc5072821d255465b08e9f1d5290
ddweard <timurnar2005@gmail.com>
2025-12-23 10:30:29 +0300

    15

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<form action="index.php" method="post">
-    <label>Enter a number to count up to:</label><br>
-    <input type="text" name="counter"><br>
-    <input type="submit" value="Start">
-</form>
-
-
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $counter = $_POST['counter'];
-    for ($i = $counter; $i >= 0; $i--) {
-        echo $i . "<br>";
+$seconds = 0;
+$running = true;
+while ($running) {
+    $seconds++;
+    echo "Seconds: {$seconds}<br>";
+    if ($seconds >= 11) { // Условный выход из цикла
+        $running = false;
     }
 }
 ?>
