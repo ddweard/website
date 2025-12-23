4841b1ad020b978a64f6b4023385ae3b3c756531
ddweard <timurnar2005@gmail.com>
2025-12-23 11:38:43 +0300

    27

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php include 'header.html'; ?>
-<h1>Home Page</h1>
-<p>Welcome to the website.</p>
-<?php include 'footer.html'; ?>
+<?php
+// Установка cookie на 1 день
+setcookie("favorite_food", "pizza", time() + 86400, "/");
+// Чтение cookie
+if (isset($_COOKIE['favorite_food'])) {
+    echo "Buy some {$_COOKIE['favorite_food']}!";
+}
+// Удаление cookie (установка времени в прошлом)
+setcookie("favorite_food", "", time() - 3600, "/");
+?>
