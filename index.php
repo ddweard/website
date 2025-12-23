c7387a04eaf88652337e68c7fc39df144d9f81df
ddweard <timurnar2005@gmail.com>
2025-12-23 11:14:47 +0300

    25

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
+<form action="index.php" method="post">
+    <label>Username:</label><br>
+    <input type="text" name="username"><br>
+    <label>E-mail:</label><br>
+    <input type="text" name="email"><br>
+    <input type="submit" value="Login">
+</form>
+
 <?php
-$username = " Bro Code ";
-$username = strtolower(trim($username));
-echo $username;
-$phone = "123-456-7890";
-$phone = str_replace("-", "", $phone);
-echo $phone; 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    if (empty($username)) {
+        echo "Username is required.";
+    }
+    if ($email === false) {
+        echo "Invalid email format.";
+    }
+}
 ?>
