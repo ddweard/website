a124f00a2b28950de78d952bd218722ff72c86b6
ddweard <timurnar2005@gmail.com>
2025-12-23 10:13:30 +0300

    4

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "I love pizza<br>";
+// Это однострочный комментарий
+/*
+Это многострочный комментарий
+*/
+?>
+<!DOCTYPE html>
+<html>
+<body>
+    <button>Order a pizza</button>
+</body>
+</html>
