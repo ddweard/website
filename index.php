04806209c04115360843a92fcc33c1b28feb904a
ddweard <timurnar2005@gmail.com>
2025-12-23 11:34:50 +0300

    26

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<form action="index.php" method="post">
-    <label>Username:</label><br>
-    <input type="text" name="username"><br>
-    <label>E-mail:</label><br>
-    <input type="text" name="email"><br>
-    <input type="submit" value="Login">
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-    if (empty($username)) {
-        echo "Username is required.";
-    }
-    if ($email === false) {
-        echo "Invalid email format.";
-    }
-}
-?>
+<?php include 'header.html'; ?>
+<h1>Home Page</h1>
+<p>Welcome to the website.</p>
+<?php include 'footer.html'; ?>
