44f8148567929ff51d54245ccf820e0bb201fc3e
ddweard <timurnar2005@gmail.com>
2025-12-23 11:39:41 +0300

    28

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-// Установка cookie на 1 день
-setcookie("favorite_food", "pizza", time() + 86400, "/");
-// Чтение cookie
-if (isset($_COOKIE['favorite_food'])) {
-    echo "Buy some {$_COOKIE['favorite_food']}!";
-}
-// Удаление cookie (установка времени в прошлом)
-setcookie("favorite_food", "", time() - 3600, "/");
+session_start();
+// Установка данных сессии
+$_SESSION['username'] = "SpongeBob";
+// Чтение данных сессии
+echo "Welcome {$_SESSION['username']}";
+// Уничтожение сессии (выход)
+session_destroy();
 ?>
