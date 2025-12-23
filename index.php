a0f14289253133cc49462c9621377a7aa532c0ff
ddweard <timurnar2005@gmail.com>
2025-12-23 11:42:15 +0300

    30

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-// Динамическое указание файла в action формы
-$current_file = htmlspecialchars($_SERVER['PHP_SELF']);
-echo "<form action='{$current_file}' method='post'>";
-// Проверка метода запроса
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    echo "Form submitted via POST.";
+$password = "pizza123";
+// Создание хеша
+$hash = password_hash($password, PASSWORD_DEFAULT);
+// Проверка пароля
+if (password_verify($password, $hash)) {
+    echo "Password is correct!";
+} else {
+    echo "Incorrect password.";
 }
 ?>
