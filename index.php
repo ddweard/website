fea1127954ba136d4095cbd2ea4667c58ee07920
ddweard <timurnar2005@gmail.com>
2025-12-23 10:17:35 +0300

    7

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<?php
-$x = 10;
-$y = 2;
-$z = $x + $y;
-echo $z;
-$counter = 0;
-$counter++;
-echo $counter;
-?>
+<form action="index.php" method="post">
+    <label>Username:</label><br>
+    <input type="text" name="username"><br>
+    <label>Password:</label><br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
 
+
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    echo "Username: {$username}<br>";
+}
+?>
