6d314930c0aaabfdbd8aebc70fcda78043f19c9c
ddweard <timurnar2005@gmail.com>
2025-12-23 10:25:53 +0300

    13

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-$grade = 'A';
-switch ($grade) {
-    case 'A':
-        echo "You did great!";
-        break;
-    case 'B':
-        echo "You did good!";
-        break;
-    case 'C':
-        echo "You did okay.";
-        break;
-    case 'D':
-        echo "You did poorly.";
-        break;
-    case 'F':
-        echo "You failed.";
-        break;
-    default:
-        echo "{$grade} is not a valid grade.";
+for ($i = 0; $i < 5; $i++) {
+    echo "Hello {$i}<br>";
 }
 ?>
