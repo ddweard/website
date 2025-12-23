a5b180c042ed0ae6d38a9ff5c877361bf33aaede
ddweard <timurnar2005@gmail.com>
2025-12-23 11:09:48 +0300

    24

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-function happyBirthday($first_name, $age) {
-    echo "Happy birthday dear {$first_name}<br>";
-    echo "You are {$age} years old!<br>";
-}
-happyBirthday("SpongeBob", 20);
+$username = " Bro Code ";
+$username = strtolower(trim($username));
+echo $username;
+$phone = "123-456-7890";
+$phone = str_replace("-", "", $phone);
+echo $phone; 
 ?>
